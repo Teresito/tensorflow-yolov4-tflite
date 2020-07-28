bad474d39b1d5fc7ddf37852f95cbf5ce7cc79dc
Teresito <Teresito.Magbag@hotmail.com>
2020-07-29 11:27:44 +1200

    checking removal of checkpoints

--- generate_mobilenetv2ssd.sh
+++ generate_mobilenetv2ssd.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 python convert_tflite.py --weights ./checkpoints/yolov4-416 --output ./checkpoints/yolov4-416.tflite
 python detectvideo.py --weights ./checkpoints/yolov4-416.tflite --size 416 --model yolov4 --video ./data/road.mp4 --framework tflite
 read -p "Press any key to continue" x
