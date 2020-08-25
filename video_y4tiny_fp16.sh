fe3516643796b633e1087b05ee02406f45cc56c2
Teresito <teresito.magbag@hotmail.com>
2020-08-25 01:54:58 +0000

    new video bash files

---
+++ video_y4tiny_fp16.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python detectvideo.py --weights ./checkpoints/yolov4-tiny_fp16.tflite --size 416 --model yolov4 --video $1 --framework tflite --tiny --output_name yolov4-tiny_fp16.mp4 $2 $3
+read -p "Press any key to continue" x
